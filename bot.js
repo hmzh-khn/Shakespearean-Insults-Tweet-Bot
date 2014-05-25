b663a8914322a4101ad521ba8b1ad088338aca67
Hamzah Khan <hkhan@newrelic.com>
2014-05-25 05:16:37 -0700

    Shakespeare insults

--- bot.js
+++ bot.js
@@ -1,103 +1,41 @@
 // Our Twitter library
 var Twit = require('twit');
-var firsthalf;
-var secondhalf;
-var tweet;
-var fulltweet;
+var q = require('q');
+var request = require('request');
 
 // We need to include our configuration file
 var T = new Twit(require('./config.js'));
 
-// This is the URL of a search for the latest tweets on the '#mediaarts' hashtag.
-var mediaArtsSearch = {q: '"Why do I"', count: 2, result_type: "recent"}; 
-var answer = {q: "Because I can", count: 2, result_type: "recent"};
-
-// This function finds the latest tweet with the #mediaarts hashtag, and retweets it.
-function retweetLatest() {
-	T.get('search/tweets', mediaArtsSearch, function (error, data) {
-	  // If our search request to the server had no errors...
-	//console.log(error, data);
-	 if(!error)
-		{
-			var tweet = data.statuses[0].text;
-			var arr = tweet.split(".");
-			var question = "";
-			var tweetnum;
-			var endindex;
-			var punctutations = ".,:;?";
-			for( var i = 0; i<arr.length; i++ )
-			{
-				var smallerarr = arr[i].split(" ");
-				for(var k = 0; k<smallerarr.length; k++)
-				{
-					if( smallerarr[k] == "Why" )
-					{
-						var index = tweet.indexOf("Why");
-					}
-					else if(smallerarr[k] == "why" )
-					{
-						var index = tweet.indexOf("why");
-					}
-					//console.log(smallerarr[k]);
-					//var regex = /[^\.\?,-\/#!@$%\^&\*;:{}=\-_`~()]/;
-					//if(!regex.test(smallerarr[k])){endindex=smallerarr[k];console.log('penis');}
-					firsthalf=tweet.substr(index);
-					//firsthalf = tweet.substring(index,endindex);	
-
+var postInsult = function() {
+	q.nfcall(request, 'http://quandyfactory.com/insult/json')
+		.then(function(resArr) {
+			var res = resArr[0];
+			if(res.statusCode == 200) {
+				var insult = JSON.parse(resArr[1]).insult;
+				return insult;
 			}
-			//console.log(firsthalf);
-			fulltweet = firsthalf;
-		}
-		//var firsthalf = arr[tweetnum];
-		T.get('search/tweets', answer, function(error,data)
-		{
-			//console.log(error,data);
-			if(!error)
-			{
-				var tweeta = data.statuses[0].text;
-				var arr = tweeta.split(".");
-				var question = "";
-				var tweetnum;
-				for( var i = 0; i<arr.length; i++ )
-				{
-					var smallerarr = arr[i].split(" ");
-					for(var k = 0; k<smallerarr.length; k++)
-					{
-						if( smallerarr[k] == "Because" )
-						{
-							var index = tweeta.indexOf("Because");
-							secondhalf = tweeta.substr(index);
-						}
-						else if(smallerarr[k] == "because" )
-						{
-							var index = tweeta.indexOf("because");
-							secondhalf = tweeta.substr(index);
-						}				
-					}
-				}
+			else {
+				return new Error('insult get failed');
 			}
-			fulltweet = fulltweet+ "? " + secondhalf;
-			T.post('statuses/update', { status: fulltweet }, function(err, reply) {
-				if(error){
-					console.log('There was a problem posting your tweet');
-				}
-				})
+		})
+
+		.then(function(insult) {
+			console.log(insult);
+
+			var tweet = "Dear #CodeDay #Chicago, "+insult+" - www.quandyfactory.com/insult (Sincerely, @CodeDay #PDX) ;)";
+			console.log(tweet.length);
+			return q.ninvoke(T, "post", 'statuses/update', { status: tweet });
+		})
+
+		.then(function(resArr) {
+			//console.log(resArr[0], resArr[1]);
+		})
+
+		.catch(function(err) {
+			console.log(err);
 		});
-		
-	  }
-	  
-	  // However, if our original search request had an error, we want to print it out here.
-	  else {
-	  	console.log('There was an error with your hashtag search:', error);
-	  }
-	});
+};
 
-	
-	
-}
+var intervalId = setInterval(postInsult, 10000);
 
-// Try to retweet something as soon as we run the program...
-retweetLatest();
-// ...and then every hour after that. Time here is in milliseconds, so
-// 1000 ms = 1 second, 1 sec * 60 = 1 min, 1 min * 60 = 1 hour --> 1000 * 60 * 60
-setInterval(retweetLatest, 1000 * 60*5);
+setTimeout(function() { clearInterval(intervalId); }, 1000*60*10);
--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // Put your own Twitter App keys here. See README.md for more detail.
 module.exports = {
-  consumer_key:         'Yp7FqJBg8CAMIsMhjJHeTQ',
-  consumer_secret:      'cxaF9NhuvTd9M2M2oOnvoM0E8DhE2Kdqe1lELlolOA',
-  access_token:         '1931970734-V7qnABeJFkPR0mnk2tbIjWuZLPsBMNnQ1B7lxVj',
-  access_token_secret:  'UTldr7lJsu2nHV18V93BYN19nCEe3e3JOpQQV3Cq8'
-}
+  consumer_key:         'LQR7R7avArbJoDZfdMKEKOeKr',
+  consumer_secret:      'lWmKYO8FtXgZm4qfeOuUOSh2h4mHs8ktsppHvZqt6Wxl5aHEfT',
+  access_token:         '2522240126-vcNljVZ5Hy1HjbfWhVsJKzKCTYaO85qy1DzV7Ml',
+  access_token_secret:  'A3WPXhV4xtJWOms3xQm8UrglAPeah5uBqFc16aWIxBFaa'
+};
